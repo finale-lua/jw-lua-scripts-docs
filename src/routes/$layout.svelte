875f5dbcdc0afb67a7c3654040fc8bc3d267ee48
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-03-30 01:05:16 -0700

    Initial commit

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import '@nick-mazuk/ui-config/colors.css'
+    import '@nick-mazuk/ui-config/styles.css'
+    import HeaderBrand from '../components/header-brand.svelte'
+    import Header from '../components/header.svelte'
+    import HeaderLink from '../components/header-link.svelte'
+</script>
+
+<Header sticky>
+    <svelte:fragment slot="left">
+        <HeaderBrand text="JW Lua Scripts" />
+    </svelte:fragment>
+    <svelte:fragment slot="right">
+        <HeaderLink href="/library" breakpoint="none">Library Docs</HeaderLink>
+    </svelte:fragment>
+</Header>
+<slot />
---
+++ src/routes/library/$layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import '@nick-mazuk/ui-config/colors.css'
+    import '@nick-mazuk/ui-config/styles.css'
+</script>
+
+<div class="flex md:space-x-12 wrapper my-16">
+    <aside class="hidden md:block w-40 lg:w-64 flex-none">
+        <nav class="sticky top-24 max-h-screen overflow-scroll pb-24"></nav>
+    </aside>
+    <slot />
+</div>
