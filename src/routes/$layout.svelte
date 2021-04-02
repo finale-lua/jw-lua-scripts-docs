3ced8d32269a1cecd3f2a93a3537224b1e2fe307
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-04-02 10:32:26 -0700

    Add footer

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -4,8 +4,13 @@
     import HeaderBrand from '@nick-mazuk/ui-svelte/src/layouts/header/header-brand/header-brand.svelte'
     import Header from '@nick-mazuk/ui-svelte/src/layouts/header/header.svelte'
     import HeaderLink from '@nick-mazuk/ui-svelte/src/layouts/header/header-link/header-link.svelte'
-        import Github from '@nick-mazuk/ui-svelte/src/elements/icon/github.svelte'
-        import LoadingProgressIndicator from '@nick-mazuk/ui-svelte/src/elements/loading-progress-indicator/loading-progress-indicator.svelte'
+    import Github from '@nick-mazuk/ui-svelte/src/elements/icon/github.svelte'
+    import LoadingProgressIndicator from '@nick-mazuk/ui-svelte/src/elements/loading-progress-indicator/loading-progress-indicator.svelte'
+    import Footer from '@nick-mazuk/ui-svelte/src/layouts/footer/footer.svelte'
+    import FooterSocialLink from '@nick-mazuk/ui-svelte/src/layouts/footer/footer-social-link/footer-social-link.svelte'
+    import DarkModeSelect from '../lib/components/site/dark-mode-select.svelte'
+
+    const githubRepoUrl = 'https://github.com/Nick-Mazuk/jw-lua-scripts'
 </script>
 
 <LoadingProgressIndicator />
@@ -16,14 +21,21 @@
     </svelte:fragment>
     <svelte:fragment slot="right">
         <HeaderLink href="/docs/getting-started" breakpoint="none">Docs</HeaderLink>
-        <HeaderLink href="https://github.com/Nick-Mazuk/jw-lua-scripts" breakpoint="none"
-            ><Github />
-        </HeaderLink>
-        <!-- <HeaderItemWrapper breakpoint="none">
-            <div class="w-32 flex items-center">
-                <DarkModeSelect />
-            </div>
-        </HeaderItemWrapper> -->
+        <HeaderLink href="{githubRepoUrl}" breakpoint="none"><Github /></HeaderLink>
     </svelte:fragment>
 </Header>
 <slot />
+
+<Footer>
+    <svelte:fragment slot="copyright">
+        ©{new Date().getFullYear()} Nick Mazuk.
+    </svelte:fragment>
+    <svelte:fragment slot="social-links">
+        <FooterSocialLink href="{githubRepoUrl}">
+            <Github />
+        </FooterSocialLink>
+    </svelte:fragment>
+    <svelte:fragment slot="theme-toggle">
+        <DarkModeSelect />
+    </svelte:fragment>
+</Footer>
