7b3535d3b6cebd4544f338eff298519bb6e06f57
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-04-05 16:53:06 -0700

    Complete site search

--- src/lib/components/site-search.svelte
+++ src/lib/components/site-search.svelte
@@ -1,78 +1,67 @@
 <script lang="ts">
+    import SearchInput from '@nick-mazuk/ui-svelte/src/form/search-input/search-input.svelte'
+    import HeaderItemWrapper from '@nick-mazuk/ui-svelte/src/layouts/header/header-item-wrapper/header-item-wrapper.svelte'
+
+    let value = ''
+
+    type Excerpt = {
+        text: string
+        highlight_ranges: { beginning: number; end: number }[]
+        score: number
+        internal_annotations: []
+        fields: {}
+    }
     type Result = {
         entry: { url: string; title: string }
-        excerpts: {
-            text: string
-            highlight_ranges: { beginning: number; end: number }[]
-            score: number
-            internal_annotations: []
-            fields: {}
-        }[]
+        excerpts: Excerpt[]
     }
 
     let results: Result[] = []
-    const downloadStork = () => {
+    let resultsMap: { [title: string]: Result }
+    let isLoading = false
+    let hideOptions = true
+    $: {
+        if (typeof window !== 'undefined' && value) {
+            results = window.stork.search('docs', value).results
+            resultsMap = {}
+            results.forEach((result) => {
+                resultsMap[result.entry.title] = result
+            })
+        } else {
+            results = []
+            resultsMap = {}
+        }
+        isLoading = value.length < 3
+        hideOptions = value.length === 0
+    }
+    const handleFocus = () => {
         window.stork.initialize('/stork.wasm')
         window.stork.downloadIndex('docs', '/stork.st')
-        console.log(stork.search)
-    }
-
-    const handleChange: svelte.JSX.FormEventHandler<HTMLInputElement> = (event) => {
-        results = window.stork.search('docs', event.currentTarget.value).results
-    }
-
-    const hello = {
-        results: [
-            {
-                entry: { url: '/docs/library/configuration', title: 'Configuration', fields: {} },
-                excerpts: [
-                    {
-                        text:
-                            '| string | the file name of the config file (which will be prepended with the',
-                        highlight_ranges: [{ beginning: 32, end: 38 }],
-                        score: 125,
-                        internal_annotations: [],
-                        fields: {},
-                    },
-                    {
-                        text: 'Configuration Author: Robert Patterson Date: March 5, 2021',
-                        highlight_ranges: [{ beginning: 1, end: 14 }],
-                        score: 118,
-                        internal_annotations: [],
-                        fields: {},
-                    },
-                    {
-                        text:
-                            'embedded within tables is not supported. get_parameters get_parameters configuration.get_parameters(file_name, parameter_list) | Input | Type | Description',
-                        highlight_ranges: [{ beginning: 71, end: 109 }],
-                        score: 93,
-                        internal_annotations: [],
-                        fields: {},
-                    },
-                ],
-                title_highlight_ranges: [{ beginning: 0, end: 13 }],
-                score: 200,
-            },
-        ],
-        total_hit_count: 1,
-        url_prefix: '',
     }
 </script>
 
-<!-- data-stork="docs" -->
-<div class="flex items-center w-full relative">
-    <input
-        type="text"
-        class="text-sm bg-transparent w-full rounded text-gray-900 border-gray-300 hover:border-gray focus:border-primary focus:ring-primary transition-colors"
-        on:input="{handleChange}"
-        on:focus|once="{downloadStork}"
-    />
-    <div class="absolute bg-background border rounded-lg w-96 h-16 top-14 shadow-lg">
-        {#each results as result}
-            <a sveltekit:prefetch href="{result.entry.url}"><p>{result.entry.title}</p></a>
-        {/each}
+<HeaderItemWrapper breakpoint="sm">
+    <div class="flex items-center flex-shrink">
+        <SearchInput
+            isLoading="{isLoading}"
+            hideOptions="{hideOptions}"
+            on:focus="{handleFocus}"
+            bind:value
+            options="{results.map((result) => result.entry.title)}"
+            width="{72}"
+        >
+            <a
+                sveltekit:prefetch
+                slot="option"
+                href="{resultsMap[option].entry.url}"
+                class="block rounded px-3 py-2 group text-left text-sm hover:bg-gray-50"
+                let:option
+            >
+                {option}
+            </a>
+        </SearchInput>
     </div>
-</div>
+</HeaderItemWrapper>
 
 <svelte:head>
     <script src="https://files.stork-search.net/stork.js" defer></script>
