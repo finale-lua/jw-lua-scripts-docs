8be3c4f15a4d9b94aa140efa731b8cb82a44b6aa
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-25 18:03:54 -0700

    refactor layout resets

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,15 @@
 <script>
     import { page } from '$app/stores'
 
-    import '@nick-mazuk/ui-config/lib/colors.css'
-    import '@nick-mazuk/ui-config/lib/styles.css'
+    import ResetItems from '$lib/components/reset-items.svelte'
+
     import HeaderBrand from '@nick-mazuk/ui-svelte/src/components/header/header-brand/header-brand.svelte'
     import Header from '@nick-mazuk/ui-svelte/src/components/header/header.svelte'
     import HeaderLink from '@nick-mazuk/ui-svelte/src/components/header/header-link/header-link.svelte'
     import HeaderMobileLink from '@nick-mazuk/ui-svelte/src/components/header/header-mobile-link/header-mobile-link.svelte'
     import HeaderMobileSubgroup from '@nick-mazuk/ui-svelte/src/components/header/header-mobile-subgroup/header-mobile-subgroup.svelte'
     import Github from '@nick-mazuk/ui-svelte/src/elements/icon/github.svelte'
-    import LoadingProgressIndicator from '@nick-mazuk/ui-svelte/src/elements/loading-progress-indicator/loading-progress-indicator.svelte'
+
     import Footer from '@nick-mazuk/ui-svelte/src/components/footer/footer.svelte'
     import FooterSocialLinkWrapper from '@nick-mazuk/ui-svelte/src/components/footer/footer-social-link-wrapper/footer-social-link-wrapper.svelte'
     import FooterSocialLink from '@nick-mazuk/ui-svelte/src/components/footer/footer-social-link/footer-social-link.svelte'
@@ -19,13 +19,14 @@
     import { libraryPages } from '../lib/lib/library-pages'
 
     const githubRepoUrl = 'https://github.com/Nick-Mazuk/jw-lua-scripts'
+
 </script>
 
 <svelte:head>
     <title>JW Lua Scripts</title>
 </svelte:head>
 
-<LoadingProgressIndicator />
+<ResetItems />
 
 <Header sticky page="{$page.path}">
     <HeaderBrand text="JW Lua Scripts" slot="left" />
