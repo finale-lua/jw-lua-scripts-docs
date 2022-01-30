cafdace086eae52fe491254747c73cabf58e13e3
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2022-01-29 16:21:30 -0800

    add macOS and Windows install redirects for backwards compat

---
+++ src/routes/help/install/macos.svelte
@@ -0,0 +1,8 @@
+<script context="module" lang="ts">
+    export const load = () => {
+        return {
+            redirect: '/help/install?os=macOS',
+            status: 301,
+        }
+    }
+</script>
---
+++ src/routes/help/install/windows.svelte
@@ -0,0 +1,8 @@
+<script context="module" lang="ts">
+    export const load = () => {
+        return {
+            redirect: '/help/install?os=Windows',
+            status: 301,
+        }
+    }
+</script>
