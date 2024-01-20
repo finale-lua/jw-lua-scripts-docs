db9be351124346b8f11755e63c83debf2e5ef0c7
Aaron Sherber <aaron@sherber.com>
2024-01-20 14:56:47 -0500

    Add support for headers

--- src/lib/components/script-notes/script-notes.svelte
+++ src/lib/components/script-notes/script-notes.svelte
@@ -6,7 +6,12 @@
 
     type Block =
         | {
-              type: 'p' | 'code'
+              type: 'p'
+              content: string
+              style: string
+          }
+        | {
+              type: 'code'
               content: string
           }
         | {
@@ -17,6 +22,7 @@
     type Content = Block[]
 
     let content: Content = []
+
     $: {
         const lines = notes.split('\n\n')
         content = lines.map((line) => {
@@ -42,9 +48,15 @@
                     }),
                 }
             } else {
+                let classString = ''
+                if (line.startsWith('###')) classString = 'text-lg font-bold'
+                else if (line.startsWith('##')) classString = 'text-xl font-bold'
+                else if (line.startsWith('#')) classString = 'text-2xl font-bold'
+
                 return {
                     type: 'p',
-                    content: line,
+                    content: line.replace(/#+\s*/, ''),
+                    style: classString,
                 }
             }
         })
@@ -54,7 +66,7 @@
 <div class="my-3 flex flex-col space-y-2">
     {#each content as block}
         {#if block.type == 'p'}
-            <p>
+            <p class="{block.style}">
                 <Leaf text="{block.content}" />
             </p>
         {:else if block.type == 'ul'}
